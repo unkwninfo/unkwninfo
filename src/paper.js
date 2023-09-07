0668f785181b2764255397faf385309b433dec31
unkwninfo <unkwninfo@gmail.com>
2023-09-08 02:06:16 +0800

    Update paper markdown

--- src/paper.js
+++ src/paper.js
@@ -1,23 +1,37 @@
 /**
  * @author unkwninfo
- * @todo markdown css config
+ * @todo markdown Editor Viewer
  */
 
+import { Viewer } from "bytemd";
+import breaks from "@bytemd/plugin-breaks";
+import frontmatter from "@bytemd/plugin-frontmatter";
+import gemoji from "@bytemd/plugin-gemoji";
+import gfm from "@bytemd/plugin-gfm";
+import highlight from "@bytemd/plugin-highlight";
+import math from "@bytemd/plugin-math";
+import mermaid from "@bytemd/plugin-mermaid";
+
 import "./paper.css";
-import MarkdownIt from "markdown-it";
+// import "highlight.js/styles/default.css";
+// import "katex/dist/katex.css";
 
 export default class Paper {
   constructor() {
     this.root = document.createElement("div");
-    this.md = new MarkdownIt();
   }
   init() {
     this.root.classList.add("paper");
-    this.root.classList.add("retro");
     fetch("/test.md")
       .then((response) => response.text())
       .then((text) => {
-        this.root.innerHTML = this.md.render(text);
+        new Viewer({
+          target: this.root,
+          props: {
+            value: text,
+            plugins: [breaks(), frontmatter(), gemoji(), gfm(), highlight(), math(), mermaid()],
+          },
+        });
       })
       .catch((err) => console.log("Request Failed", err));
   }
